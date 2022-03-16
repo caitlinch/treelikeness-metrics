3130c48a556f12e3ab8f2ff4ba1038f0e94abd1f
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-03-16 16:07:14 +1100

    Add mean q residual and mean tiger value to output

--- 02_apply_treelikeness_metrics.R
+++ 02_apply_treelikeness_metrics.R
@@ -86,9 +86,9 @@ treelikeness.metrics.simulations <- function(replicate_folder, iqtree2_path, spl
   # Apply tree proportion (new test)
  
   ## Assemble results into a dataframe
-  results_vec <- c(lm, scfs$mean_scf, scfs$median_scf, min(scfs$all_scfs), max(scfs$all_scfs), mean_delta_plot_value)
+  results_vec <- c(lm, scfs$mean_scf, scfs$median_scf, min(scfs$all_scfs), max(scfs$all_scfs), mean_delta_plot_value, mean_q_residual, mean_tiger_value)
   names_vec <- c("lm_num_resolved_quartets", "lm_num_partly_resolved_quartets", "lm_num_unresolved_quartets", "lm_total_num_quartets", "lm_proportion_resolved_quartets",
-                 "scf_mean", "scf_median", "scf_min", "scf_max", "mean_delta_plot_value")
+                 "scf_mean", "scf_median", "scf_min", "scf_max", "mean_delta_plot_value", "mean_q_residual", "mean_tiger_value")
   names(results_vec) <- names_vec
   
    

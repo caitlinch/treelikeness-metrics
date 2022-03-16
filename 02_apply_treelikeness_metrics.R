52ff8886a76f208c8c4b8ae9831dfc8df7460c8b
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-03-16 15:41:16 +1100

    Added all complete treelikeness tests

--- 02_apply_treelikeness_metrics.R
+++ 02_apply_treelikeness_metrics.R
@@ -32,22 +32,50 @@ exp_folders <- paste0(local_directory, c("exp_1/", "exp_2/", "exp_3/"))
 exp1_runs <- paste0(exp_folders[1], list.files(exp_folders[1]), "/")
 replicate_folder <- exp1_runs[1]
 
-treelikeness.metrics.simulations <- function(replicate_folder, iqtree2_path, fast_TIGER_path, phylogemetric_path, splitstree_path, 
-                                             sequence_format = "DNA", number_of_threads_for_tests = NA){
+treelikeness.metrics.simulations <- function(replicate_folder, iqtree2_path, splitstree_path, phylogemetric_path, fast_TIGER_path,
+                                             num_iqtree2_threads = "AUTO", num_iqtree2_scf_quartets = 100, num_phylogemetric_threads = NA,
+                                             sequence_format = "DNA"){
   ## Function to take one alignment and apply all treelikeness metrics
   
   # Get alignment file
   folder_files <- list.files(replicate_folder)
   alignment_path <- paste0(replicate_folder, grep("output_alignment.fa.", grep("output_alignment.fa", folder_files, value = TRUE), invert = TRUE, value = TRUE))
   
+  # Determine the number of taxa (needed for number of quartets in likelihood mapping and sCFs)
+  if (grepl("exp1", replicate_folder)){
+    random_trees_file <- paste0(replicate_folder, grep("random_trees", folder_files, value = TRUE))
+    random_trees <- read.tree(random_trees_file)
+    n_tree_tips <- unique(Ntip(random_trees))[[1]]
+  } else if ((grepl("exp2", replicate_folder))|(grepl("exp3", replicate_folder))){
+    start_tree_file <- paste0(replicate_folder, grep("starting_tree", folder_files, value = TRUE))
+    start_tree <- read.tree(start_tree_file)
+    n_tree_tips <- Ntip(start_tree)
+  }
+  
+  # Apply Likelihood mapping (Strimmer and von Haeseler 1997)
+  lm <- likelihood.mapping(alignment_path, iqtree2_path, iqtree2_number_threads = num_iqtree2_threads, number_of_taxa = n_tree_tips)
+  
+  # Apply Site concordance factors (Minh et. al. 2020)
+  scfs <- scf(alignment_path, iqtree2_path, iqtree2_number_threads = num_iqtree2_threads, number_scf_quartets = num_iqtree2_scf_quartets, 
+              add.likelihood.map = FALSE, number_of_taxa = n_tree_tips)
+  
+  # Apply Delta plots (Holland et. al. 2002)
+  mean_delta_plot_value <- mean.delta.plot.value(alignment_path, sequence_format, substitution_model = "JC69")
+  
   # Apply Network Treelikeness Test (Huson and Bryant 2006)
   ntlt <- network.treelikeness.test(alignment_path, splitstree_path, sequence_format)
   
   # Apply Q-residuals (Gray et. al. 2010)
-  mean_q_residual <- q_residuals(alignment_path, phylogemetric_path, sequence_format, phylogemetric_number_of_threads = number_of_threads_for_tests)
+  mean_q_residual <- q_residuals(alignment_path, phylogemetric_path, sequence_format, phylogemetric_number_of_threads = num_phylogemetric_threads)
   
   # Apply TIGER (Cummins and McInerney 2011)
   mean_tiger_value <- TIGER(alignment_path, fast_TIGER_path, sequence_format)
-    
+  
+  # Apply Treeness test (Cavalli-Sforza and Piazza 1975)
+  
+  # Apply Cunningham test (Cunningham 1975)
+  
+  # Apply tree proportion (new test)
+  
 }
 

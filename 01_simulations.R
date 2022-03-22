099ed5b00f6742a24ba256731aa3600ee0c2a8c5
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-03-22 14:33:31 +1100

    Added tree depth (coalescent depths from literature)

--- 01_simulations.R
+++ 01_simulations.R
@@ -13,17 +13,18 @@ library(phytools)
 
 
 #### 2. Set parameters ####
-# local_directory         <- Directory where alignments will be saved/treelikeness metrics will be run.
-# repo_directory          <- Location of caitlinch/treelikeness_metrics github repository (for access to functions).
-# iqtree2_path            <- Path to IQ-Tree2.2-beta executable (this is the IQ-Tree2 release containing Alisim). 
-# total_alignment_length  <- Total length of concatenated alignments (we chose 10000).
-# sequence_type           <- Sequence type for simulation (we chose "DNA").
-# taxa_vec                <- Number of taxa to simulate (we chose 10,20,50,100,200,500, and 1000).
-# num_reps                <- Number of replicates to run for each set of simulation conditions (we chose 10). Must be >= 1.
-# tree_depth              <- Single value or vector of values for total tree length
-# r_vec                   <- Values of introgression (we chose from 0 to 1 in intervals of 0.05)
-# alisim_gene_models      <- model of sequence evolution for Alisim 
-# alisim_gene_tree_length <- gene-specific tree length for Alisim
+# local_directory                 <- Directory where alignments will be saved/treelikeness metrics will be run.
+# repo_directory                  <- Location of caitlinch/treelikeness_metrics github repository (for access to functions).
+# iqtree2_path                    <- Path to IQ-Tree2.2-beta executable (this is the IQ-Tree2 release containing Alisim). 
+# total_alignment_length          <- Total length of concatenated alignments (we chose 10000).
+# sequence_type                   <- Sequence type for simulation (we chose "DNA").
+# taxa_vec                        <- Number of taxa to simulate (we chose 10,20,50,100,200,500, and 1000).
+# num_reps                        <- Number of replicates to run for each set of simulation conditions (we chose 10). Must be >= 1.
+# tree_depth_random               <- One or more values for the tree length of randomly generated trees.
+# tree_depth_coalescent_bounds    <- Two values for minimum and maximum tree length of coalescent trees.
+# r_vec                           <- Values of introgression (we chose from 0 to 1 in intervals of 0.05)
+# alisim_gene_models              <- model of sequence evolution for Alisim 
+# alisim_gene_tree_length         <- gene-specific tree length for Alisim
 
 local_directory <- "/Users/caitlincherryh/Documents/C2_TreelikenessMetrics/"
 repo_directory <- "/Users/caitlincherryh/Documents/Repositories/treelikeness_metrics/"
@@ -33,16 +34,22 @@ total_alignment_length <- 10000
 sequence_type <- "DNA"
 taxa_vec <- c(10,20,50,100,200,500,1000)
 num_reps <- 10
-tree_depth <- 1
+tree_depth_random <- c(0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5)
+tree_depth_coalescent_bounds <- c(0.25, 70) # where bounds for coalescent tree depth are 0.25 (minimum) and 70 (maximum) in coalescent units
 r_vec <- seq(0, 0.5, 0.05)
 alisim_gene_models <- "JC"
 alisim_gene_tree_length <- NA
 
 ## Prepare variables that remain stable for all experiments using the input parameters
+# Determine number of trees - all whole numbers that are a divisor of the total alignment length
 number_of_trees <- divisors(total_alignment_length)
+# Set number of taxa equal to taxa_vec
 number_of_taxa <- taxa_vec
+# Create a list of all replicate numbers using the num_reps value
 number_of_replicates <- 1:num_reps
-
+# Prepare vector for depth of coalescent trees
+tree_depth_coalescent <- seq(floor(tree_depth_coalescent_bounds[1]),ceiling(tree_depth_coalescent_bounds[2]), (ceiling(tree_depth_coalescent_bounds[2])/10))
+tree_depth_coalescent[1] <- tree_depth_coalescent_bounds[1] # Replace first value with actual first value
 
 
 #### 3. Source functions from caitlinch/treelikeness_metrics ####
@@ -64,7 +71,7 @@ exp1_dir <- paste0(local_directory, "exp_1/")
 if(!file.exists(exp1_dir)){dir.create(exp1_dir)}
 
 # Create matrix with parameters for generating each simulated alignment
-exp1_params <- expand.grid(number_of_replicates, number_of_taxa, number_of_trees, tree_depth)
+exp1_params <- expand.grid(number_of_replicates, number_of_taxa, number_of_trees, tree_depth_random)
 names(exp1_params) <- c("num_reps", "num_taxa", "num_trees", "tree_depth")
 # Add a unique identifier (uid) of the form: experiment_`number of trees`_`number of taxa`_`replicate number`
 exp1_params$uid <- paste0("exp1_",sprintf("%05d", exp1_params$num_trees), "_", sprintf("%04d", exp1_params$num_taxa), "_",
@@ -100,7 +107,7 @@ if(!file.exists(exp2_dir)){dir.create(exp2_dir)}
 # Prepare parameters for experiments
 # This experiment differs because the number of NNI moves is a variable included in the expand.grid function
 # Create matrix with parameters for generating each simulated alignment
-exp2_params <- expand.grid(number_of_replicates, number_of_taxa, number_of_trees, tree_depth)
+exp2_params <- expand.grid(number_of_replicates, number_of_taxa, number_of_trees, tree_depth_coalescent)
 names(exp2_params) <- c("num_reps", "num_taxa", "num_trees", "tree_depth")
 # Add a unique identifier (uid) of the form: experiment_`number of trees`_`number of taxa`_`replicate number`
 exp2_params$uid <- paste0("exp2_",sprintf("%05d", exp2_params$num_trees), "_", sprintf("%04d", exp2_params$num_taxa), "_",
@@ -132,7 +139,7 @@ lapply(1, ms.generate.alignment, output_directory = exp2_dir, ms_path = ms_path,
 exp3_dir <- paste0(local_directory, "exp_3/")
 if (dir.exists(exp3_dir) == FALSE){dir.create(exp3_dir)}
 
-exp3_params <- expand.grid(number_of_replicates, number_of_taxa, number_of_trees, tree_depth, r_vec, c("Ancient","Recent"))
+exp3_params <- expand.grid(number_of_replicates, number_of_taxa, number_of_trees, tree_depth_coalescent, r_vec, c("Ancient","Recent"))
 names(exp3_params) <- c("num_reps", "num_taxa", "num_trees", "tree_depth", "recombination_value", "recombination_type")
 # Add a unique identifier (uid) of the form: experiment_`number of trees`_`number of taxa`_`replicate number`
 exp3_params$uid <- paste0("exp3_",sprintf("%05d", exp3_params$num_trees), "_", sprintf("%04d", exp3_params$num_taxa), "_",

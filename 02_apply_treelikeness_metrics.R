795141a299b58b5f98e7a0e88d51de654f09222c
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-05-10 15:36:23 +1000

    Updated code for checking run %

--- 02_apply_treelikeness_metrics.R
+++ 02_apply_treelikeness_metrics.R
@@ -128,20 +128,24 @@ write(exp2_apply_metrics, paste0(local_directory, "exp2_missing_results.csv"))
 #                                  supply_number_of_taxa = FALSE, number_of_taxa = NA, num_iqtree2_threads = "AUTO", num_iqtree2_scf_quartets = 100, 
 #                                  iqtree_substitution_model = "JC", distance_matrix_substitution_method = "JC69", num_phylogemetric_threads = NA, 
 #                                  tree_proportion_remove_trivial_splits = TRUE, sequence_format = "DNA", return_collated_data = TRUE)
-#
+
 # # To check number of completed runs:
 # d <- "/data/caitlin/treelikeness_metrics/exp_1/"
 # all_files <- list.files(d, recursive = TRUE)
 # aln_files <- grep("_output_alignment", all_files, value = TRUE)
 # als <- grep(".fa.",aln_files, value = TRUE, invert = TRUE)
 # col_files <- grep("collated", all_files, value = TRUE)
-# tl_files <- grep("treelikeness_metrics", all_files, value = TRUE)
 # tl_files <- grep("treelikeness_results", all_files, value = TRUE)
+# print(length(als))
+# print(length(tl_files))
+# print(paste0(length(tl_files)/length(als)*100, " % Experiment 1 alignments completed"))
 # d <- "/data/caitlin/treelikeness_metrics/exp_2/"
 # all_files <- list.files(d, recursive = TRUE)
 # aln_files <- grep("_output_alignment", all_files, value = TRUE)
 # als <- grep(".fa.",aln_files, value = TRUE, invert = TRUE)
 # col_files <- grep("collated", all_files, value = TRUE)
-# tl_files <- grep("treelikeness_metrics", all_files, value = TRUE)
 # tl_files <- grep("treelikeness_results", all_files, value = TRUE)
+# print(length(als))
+# print(length(tl_files))
+# print(paste0(length(tl_files)/length(als)*100, " % Experiment 2 alignments completed"))
 

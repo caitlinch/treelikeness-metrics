6d05db1b9941ca10ae3d71cce84e42a6d8f1b1a0
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-07-07 14:26:37 +1000

    Rewrote code - now get list of alignments from output from 01_simulations (rather than get list of alignments manually with list.files). Also better collation of finished runs + better redo of missing runs

--- code/02_apply_treelikeness_metrics.R
+++ code/02_apply_treelikeness_metrics.R
@@ -63,6 +63,7 @@ library(parallel)
 
 # Source functions from caitlinch/treelikeness_metrics
 source(paste0(repo_directory, "code/func_metrics.R"))
+source(paste0(repo_directory, "code/func_data_analysis.R"))
 
 # Find the folders of simulated alignments
 exp_folders <- paste0(local_directory, c("exp_1/", "exp_2/"))
@@ -73,42 +74,52 @@ exp_folders <- paste0(local_directory, c("exp_1/", "exp_2/"))
 # For each experiment, get the list of directories within that experiment folder and apply the test statistics to the alignment within each directory
 
 if (run_exp1 == FALSE){
-  # For experiment 1:
-  exp1_all_files <- paste0(exp_folders[1], list.files(exp_folders[1], recursive = TRUE))
-  exp1_aln_files <- grep("_output_alignment", exp1_all_files, value = TRUE)
-  exp1_als <- grep(".fa.", exp1_aln_files, value = TRUE, invert = TRUE)
+  ## For experiment 1:
+  # Open output df and get names of alignments
+  exp1_op_file <- paste0(results_dir, grep("rerun", grep("exp1", grep("file_output_paths", results_files, value = TRUE), value = TRUE), value = TRUE, invert = TRUE))
+  exp1_op_df <- read.csv(exp1_op_file, stringsAsFactors = FALSE)
   # Exp1 encountering errors in all cores. Not running properly. Remove all alignments with substitution rate 1e-04 and 0.001 (too many identical sequences)
-  exp1_als <- grep("1e-04", exp1_als, value = TRUE, invert = TRUE)
-  exp1_als <- grep("0.001", exp1_als, value = TRUE, invert = TRUE)
-  # Apply treelikeness metrics to all alignments with substitution rate of 1e-02 (0.01) or higher
-  exp1_list <- mclapply(exp1_als, treelikeness.metrics.simulations, iqtree2_path, splitstree_path, phylogemetric_path, fast_TIGER_path, 
-                        supply_number_of_taxa = FALSE, number_of_taxa = NA, num_iqtree2_threads = "AUTO", 
-                        num_iqtree2_scf_quartets = 100, iqtree_substitution_model = "JC", 
-                        distance_matrix_substitution_method = "JC69", num_phylogemetric_threads = NA,
-                        tree_proportion_remove_trivial_splits = TRUE, run_splitstree_for_tree_proportion = TRUE,
-                        sequence_format = "DNA", return_collated_data = TRUE, apply.TIGER = TRUE,
-                        redo = FALSE,
-                        mc.cores = num_cores)
+  exp1_op_df <- exp1_op_df[(exp1_op_df$tree_depth != 1e-04 & exp1_op_df$tree_depth != 1e-03),]
+  # Get list of alignments
+  exp1_als <- exp1_op_df$output_alignment_file
+  # Apply treelikeness metrics to all alignments 
+  mclapply(exp1_als, treelikeness.metrics.simulations, iqtree2_path, splitstree_path, phylogemetric_path, fast_TIGER_path, 
+           supply_number_of_taxa = FALSE, number_of_taxa = NA, num_iqtree2_threads = "AUTO", 
+           num_iqtree2_scf_quartets = 100, iqtree_substitution_model = "JC", 
+           distance_matrix_substitution_method = "JC69", num_phylogemetric_threads = NA,
+           tree_proportion_remove_trivial_splits = TRUE, run_splitstree_for_tree_proportion = TRUE,
+           sequence_format = "DNA", return_collated_data = TRUE, apply.TIGER = TRUE,
+           redo = FALSE,
+           mc.cores = num_cores)
+  
+  # Collect and collate
+  exp1_list <- mclapply(exp1_als, collate.treelikeness.results, mc.cores = num_cores)
   exp1_df <- as.data.frame(do.call("rbind", exp1_list))
-  exp1_df_name <- paste0(local_directory, "exp1_treelikeness_metrics_results.csv")
+  exp1_df_name <- paste0(local_directory, "exp1_treelikeness_metrics_collated_results.csv")
   write.csv(exp1_df, exp1_df_name, row.names = FALSE)
 }
 
 if (run_exp2 == TRUE){
-  # For experiment 2:
-  exp2_all_files <- paste0(exp_folders[2], list.files(exp_folders[2], recursive = TRUE))
-  exp2_aln_files <- grep("_output_alignment", exp2_all_files, value = TRUE)
-  exp2_als <- grep(".fa.", exp2_aln_files, value = TRUE, invert = TRUE)
-  exp2_list <- mclapply(exp2_als, treelikeness.metrics.simulations, iqtree2_path, splitstree_path, phylogemetric_path, fast_TIGER_path, 
-                        supply_number_of_taxa = FALSE, number_of_taxa = NA, num_iqtree2_threads = "AUTO", 
-                        num_iqtree2_scf_quartets = 100, iqtree_substitution_model = "JC", 
-                        distance_matrix_substitution_method = "JC69", num_phylogemetric_threads = NA,
-                        tree_proportion_remove_trivial_splits = TRUE, run_splitstree_for_tree_proportion = TRUE,
-                        sequence_format = "DNA", return_collated_data = TRUE, apply.TIGER = FALSE,
-                        redo = TRUE,
-                        mc.cores = num_cores)
+  ## For experiment 2:
+  # Open output df and get names of alignments
+  exp2_op_file <- paste0(results_dir, grep("rerun", grep("exp2", grep("file_output_paths", results_files, value = TRUE), value = TRUE), value = TRUE, invert = TRUE))
+  exp2_op_df <- read.csv(exp2_op_file, stringsAsFactors = FALSE)
+  # Get list of alignments
+  exp2_als <- exp2_op_df$output_alignment_file
+  # Apply treelikeness metrics to all alignments 
+  mclapply(exp2_als, treelikeness.metrics.simulations, iqtree2_path, splitstree_path, phylogemetric_path, fast_TIGER_path, 
+           supply_number_of_taxa = FALSE, number_of_taxa = NA, num_iqtree2_threads = "AUTO", 
+           num_iqtree2_scf_quartets = 100, iqtree_substitution_model = "JC", 
+           distance_matrix_substitution_method = "JC69", num_phylogemetric_threads = NA,
+           tree_proportion_remove_trivial_splits = TRUE, run_splitstree_for_tree_proportion = TRUE,
+           sequence_format = "DNA", return_collated_data = TRUE, apply.TIGER = FALSE,
+           redo = TRUE,
+           mc.cores = num_cores)
+  
+  # Collect and collate
+  exp2_list <- mclapply(exp2_als, collate.treelikeness.results, mc.cores = num_cores)
   exp2_df <- as.data.frame(do.call("rbind", exp2_list))
-  exp2_df_name <- paste0(local_directory, "exp2_treelikeness_metrics_results.csv")
+  exp2_df_name <- paste0(local_directory, "exp2_treelikeness_metrics_collated_results.csv")
   write.csv(exp2_df, exp2_df_name, row.names = FALSE)
 }
 
@@ -124,13 +135,13 @@ results_files <- list.files(results_dir)
 exp_ids <- rerun_experiment_ids
 for (e in exp_ids){
   # Open parameters csv
-  e_params_file <- paste0(results_dir, grep(e, grep("parameters", results_files, value = TRUE), value = TRUE))
+  e_params_file <- paste0(results_dir, grep("rerun", grep(e, grep("parameters", results_files, value = TRUE), value = TRUE), value = TRUE, invert = TRUE))
   e_params_df <- read.csv(e_params_file, stringsAsFactors = FALSE)
   # Open results csv
-  e_results_file <- paste0(results_dir, grep(e, grep("treelikeness_metrics_results", results_files, value = TRUE), value = TRUE))
+  e_results_file <- paste0(results_dir, grep("rerun", grep(e, grep("treelikeness_metrics_results", results_files, value = TRUE), value = TRUE), value = TRUE, invert = TRUE))
   e_results_df <- read.csv(e_results_file, stringsAsFactors = FALSE)
   # Open output paths csv
-  e_op_file <- paste0(results_dir, grep(e, grep("file_output_paths", results_files, value = TRUE), value = TRUE))
+  e_op_file <- paste0(results_dir, grep("rerun", grep(e, grep("file_output_paths", results_files, value = TRUE), value = TRUE), value = TRUE, invert = TRUE))
   e_op_df <- read.csv(e_op_file, stringsAsFactors = FALSE)
   
   # For experiment 1, remove rows with substitution rates that are too low
@@ -187,15 +198,15 @@ for (e in exp_ids){
         e_rerun_df_name <- paste0(local_directory, e, "_treelikeness_metrics_results_collated.csv")
         write.csv(e_rerun_df, e_rerun_df_name, row.names = FALSE)
         
-    } else if (length(results_ids) > length(params_ids)){
-      # Set output id
-      output_id <- "missing.from.params"
-      
-      # Get ids of alignments missing from params df (safety check - shouldn't be possible) and save
-      missing_ids <- results_ids[!(results_ids %in% params_ids)]
-      missing_ids_file <- paste0(results_dir, e, "_uids_", output_id, ".txt") 
-      write(missing_its, file = missing_ids_file)
-
+      } else if (length(results_ids) > length(params_ids)){
+        # Set output id
+        output_id <- "missing.from.params"
+        
+        # Get ids of alignments missing from params df (safety check - shouldn't be possible) and save
+        missing_ids <- results_ids[!(results_ids %in% params_ids)]
+        missing_ids_file <- paste0(results_dir, e, "_uids_", output_id, ".txt") 
+        write(missing_its, file = missing_ids_file)
+        
       } # end (rerun_missing_runs == TRUE)
       
     } # end (length(results_ids) > length(params_ids))

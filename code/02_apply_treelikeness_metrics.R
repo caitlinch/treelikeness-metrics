9a030d9ed7f1297d12035485742af02e750c7988
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-07-07 14:52:40 +1000

    Better way to collate completed alignments, added in all three places where results are collated. Better documention, removed unneccessary variables.

--- code/02_apply_treelikeness_metrics.R
+++ code/02_apply_treelikeness_metrics.R
@@ -7,16 +7,24 @@
 
 
 #### 1. Set parameters ####
-# local_directory         <- Directory where alignments will be saved/treelikeness metrics will be run.
-# repo_directory          <- Location of caitlinch/treelikeness-metrics github repository (for access to functions).
-# iqtree2_path            <- Path to IQ-Tree2.2-beta executable (this is the IQ-Tree2 release containing Alisim). 
-# fast_TIGER_path         <- Path to fast TIGER executable.
+## Directories
+# local_directory         <- Directory where alignments will be saved/treelikeness metrics will be run
+# results_directory       <- Directory where collated results from the treelikeness test statistics will be saved
+# repo_directory          <- Location of caitlinch/treelikeness-metrics github repository (for access to functions)
+
+## Executable paths
+# iqtree2_path            <- Path to IQ-Tree2.2-beta executable (this is the IQ-Tree2 release containing Alisim)
+# fast_TIGER_path         <- Path to fast TIGER executable
 # phylogemetric_path      <- Path to phylogemetric executable
 # splitstree_path         <- Path to SplitsTree 4 version 4.17.2 or above
+
+## Run parameters
 # num_cores               <- Number of parallel threads to use at once
 
-# run_exp1             <- Whether to apply the treelikeness test statistics to the first set of alignments
-# run_exp2             <- Whether to apply the treelikeness test statistics to the second set of alignments
+## Control variables
+# run_exp1             <- Whether to apply the treelikeness test statistics to the first set of alignments (logical)
+# run_exp2             <- Whether to apply the treelikeness test statistics to the second set of alignments (logical)
+# rerun_missing_runs   <- Whether or not to rerun alignments that do not have treelikeness test metrics complete (logical)
 # rerun_experiment_ids <- Which experiments to check for incomplete runs and rerun missing alignments. 
 #                      <- To check both exp1 and exp2, set <- c("exp1", "exp2")
 
@@ -51,10 +59,11 @@ if (run_location == "local"){
   num_cores <- 50
 }
 
+# Control variables
 run_exp1 <- FALSE
 run_exp2 <- FALSE
 rerun_missing_runs <- TRUE
-rerun_experiment_ids <- c("exp1")
+rerun_experiment_ids <- c("exp2")
 
 
 #### 2. Prepare analyses ####
@@ -92,10 +101,14 @@ if (run_exp1 == FALSE){
            redo = FALSE,
            mc.cores = num_cores)
   
-  # Collect and collate
+  # Collect and collate results
   exp1_list <- mclapply(exp1_als, collate.treelikeness.results, mc.cores = num_cores)
-  exp1_df <- as.data.frame(do.call("rbind", exp1_list))
-  exp1_df_name <- paste0(local_directory, "exp1_treelikeness_metrics_collated_results.csv")
+  # Remove NULL objects in list (indicates treelikeness metrics csv does not exist for this alignment)
+  keep_indexes <- which(!sapply(exp1_list, is.null))
+  exp1_list_filtered <- exp1_list[keep_indexes]
+  # Save output dataframe
+  exp1_df <- as.data.frame(do.call("rbind", exp1_list_filtered))
+  exp1_df_name <- paste0(results_directory, "exp1_treelikeness_metrics_collated_results.csv")
   write.csv(exp1_df, exp1_df_name, row.names = FALSE)
 }
 
@@ -116,10 +129,14 @@ if (run_exp2 == TRUE){
            redo = TRUE,
            mc.cores = num_cores)
   
-  # Collect and collate
+  # Collect and collate results
   exp2_list <- mclapply(exp2_als, collate.treelikeness.results, mc.cores = num_cores)
-  exp2_df <- as.data.frame(do.call("rbind", exp2_list))
-  exp2_df_name <- paste0(local_directory, "exp2_treelikeness_metrics_collated_results.csv")
+  # Remove NULL objects in list (indicates treelikeness metrics csv does not exist for this alignment)
+  keep_indexes <- which(!sapply(exp2_list, is.null))
+  exp2_list_filtered <- exp2_list[keep_indexes]
+  # Save output dataframe
+  exp2_df <- as.data.frame(do.call("rbind", exp2_list_filtered))
+  exp2_df_name <- paste0(results_directory, "exp2_treelikeness_metrics_collated_results.csv")
   write.csv(exp2_df, exp2_df_name, row.names = FALSE)
 }
 
@@ -138,7 +155,7 @@ for (e in exp_ids){
   e_params_file <- paste0(results_dir, grep("rerun", grep(e, grep("parameters", results_files, value = TRUE), value = TRUE), value = TRUE, invert = TRUE))
   e_params_df <- read.csv(e_params_file, stringsAsFactors = FALSE)
   # Open results csv
-  e_results_file <- paste0(results_dir, grep("rerun", grep(e, grep("treelikeness_metrics_results", results_files, value = TRUE), value = TRUE), value = TRUE, invert = TRUE))
+  e_results_file <- paste0(results_dir, grep("rerun", grep(e, grep("treelikeness_metrics_collated_results", results_files, value = TRUE), value = TRUE), value = TRUE, invert = TRUE))
   e_results_df <- read.csv(e_results_file, stringsAsFactors = FALSE)
   # Open output paths csv
   e_op_file <- paste0(results_dir, grep("rerun", grep(e, grep("file_output_paths", results_files, value = TRUE), value = TRUE), value = TRUE, invert = TRUE))
@@ -174,8 +191,10 @@ for (e in exp_ids){
       write.csv(missing_als_df, file = missing_als_file, row.names = TRUE)
       
       if (rerun_missing_runs == TRUE){
+        # Set which alignments to rerun
+        rerun_al_paths <- missing_als_df$output_alignment_file
         # Run missing alignments
-        mclapply(missing_als_df$output_alignment_file, treelikeness.metrics.simulations, 
+        mclapply(rerun_al_paths, treelikeness.metrics.simulations, 
                  iqtree2_path, splitstree_path, phylogemetric_path, fast_TIGER_path, 
                  supply_number_of_taxa = FALSE, number_of_taxa = NA, num_iqtree2_threads = "AUTO", 
                  num_iqtree2_scf_quartets = 100, iqtree_substitution_model = "JC", 
@@ -184,19 +203,16 @@ for (e in exp_ids){
                  sequence_format = "DNA", return_collated_data = TRUE, apply.TIGER = TRUE,
                  redo = FALSE,
                  mc.cores = num_cores)
-        # Collate all alignments 
-        e_list <- mclapply(missing_als_df$output_alignment_file, treelikeness.metrics.simulations, 
-                           iqtree2_path, splitstree_path, phylogemetric_path, fast_TIGER_path, 
-                           supply_number_of_taxa = FALSE, number_of_taxa = NA, num_iqtree2_threads = "AUTO", 
-                           num_iqtree2_scf_quartets = 100, iqtree_substitution_model = "JC", 
-                           distance_matrix_substitution_method = "JC69", num_phylogemetric_threads = NA,
-                           tree_proportion_remove_trivial_splits = TRUE, run_splitstree_for_tree_proportion = TRUE,
-                           sequence_format = "DNA", return_collated_data = TRUE, apply.TIGER = TRUE,
-                           redo = FALSE,
-                           mc.cores = num_cores)
-        e_rerun_df <- as.data.frame(do.call("rbind", e_list))
-        e_rerun_df_name <- paste0(local_directory, e, "_treelikeness_metrics_results_collated.csv")
-        write.csv(e_rerun_df, e_rerun_df_name, row.names = FALSE)
+ 
+        # Collect and collate results
+        e_list <- mclapply(rerun_al_paths, collate.treelikeness.results, mc.cores = num_cores)
+        # Remove NULL objects in list (indicates treelikeness metrics csv does not exist for this alignment)
+        keep_indexes <- which(!sapply(e_list, is.null))
+        e_list_filtered <- e_list[keep_indexes]
+        # Save output dataframe
+        e_df <- as.data.frame(do.call("rbind", e_list_filtered))
+        e_df_name <- paste0(results_directory, e, "_treelikeness_metrics_collated_results.csv")
+        write.csv(e_df, e_df_name, row.names = FALSE)
         
       } else if (length(results_ids) > length(params_ids)){
         # Set output id

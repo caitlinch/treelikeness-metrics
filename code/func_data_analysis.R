9a13a89da6d4360c199060fd4ae7e6c95d9a5325
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-07-22 13:22:51 +1000

    Added new function to determine proportion of treelike alignments for exp2 (need to look up different columns based in input parameters)

--- code/func_data_analysis.R
+++ code/func_data_analysis.R
@@ -45,7 +45,8 @@ collate.treelikeness.results <- function(alignment_path, experiment_number){
   return(collated_df)
 }
 
-reformat.network.treelikeness.test.results <- function(id, params_df, results_df){
+reformat.network.treelikeness.test.results.exp1 <- function(id, params_df, results_df){
+  # For processing experiment 1 results
   # Function to collect the proportion of treelike alignments for each set of parameters
   # Reformat network treelikeness test results for pretty plotting
   
@@ -56,6 +57,30 @@ reformat.network.treelikeness.test.results <- function(id, params_df, results_df
   row_results_df <- results_df[(results_df$num_taxa == row$num_taxa & 
                                   results_df$num_trees == row$num_trees & 
                                   results_df$tree_depth == row$tree_depth), ]
+  # Find number of treelike and non-treelike results
+  tl_results <- length(which(row_results_df$NetworkTreelikenessTest == "Treelike"))
+  ntl_results <- length(which(row_results_df$NetworkTreelikenessTest == "Non-treelike"))
+  # Find total number of alignments with these parameters
+  n_als <- nrow(row_results_df)
+  # Find proportion of treelike results
+  prop_tl_als <- tl_results/n_als
+  # Return the proportion of treelike alignments for this set of parameter values
+  return(prop_tl_als)
+}
+
+reformat.network.treelikeness.test.results.exp2 <- function(id, params_df, results_df){
+  # For processing experiment 2 results
+  # Function to collect the proportion of treelike alignments for each set of parameters
+  # Reformat network treelikeness test results for pretty plotting
+  
+  # Extract relevant row
+  row <- params_df[id, ]
+  
+  # Filter results_df to only rows including the relevent params
+  row_results_df <- results_df[(results_df$num_taxa == row$num_taxa & 
+                                  results_df$tree_depth == row$tree_depth &
+                                  results_df$recombination_value == row$recombination_value &
+                                  results_df$recombination_type == row$recombination_type), ]
   # Find number of treelike and non-treelike results
   tl_results <- length(which(row_results_df$NetworkTreelikenessTest == "Treelike"))
   ntl_results <- length(which(row_results_df$NetworkTreelikenessTest == "Non-treelike"))

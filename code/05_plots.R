1777d73842363abbb8586fe5eee91c457b86a5d7
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-08-05 13:22:13 +1000

    Added timing plots

--- code/05_plots.R
+++ code/05_plots.R
@@ -25,9 +25,6 @@ library(reshape2)
 library(ggplot2)
 library(patchwork)
 
-# List all data files
-data_files <- paste0(data_directory, list.files(data_directory))
-
 # Create folder for plots
 plot_directory <- paste0(output_directory, "plots/")
 if (dir.exists(plot_directory) == FALSE){dir.create(plot_directory)}
@@ -35,12 +32,13 @@ if (dir.exists(plot_directory) == FALSE){dir.create(plot_directory)}
 
 
 #### 3. Plot timings for experiment 2 ####
+# List all data files
+data_files <- paste0(data_directory, list.files(data_directory))
 # Identify the experiment 2 timings file
-time_file <- grep("exp2", grep("test_statistic_timing", data_files, value = TRUE), value = TRUE)
-# Open the timings file
-time_df <- read.csv(time_file)
-# Add a column with the number of gene trees
-time_df$num_genes <- 250
+time_files <- grep("exp2", grep("test_statistic_timing", data_files, value = TRUE), value = TRUE)
+# Open the timings file(s) and collate them into one dataframe
+time_list <- lapply(time_files, read.csv)
+time_df <- as.data.frame(do.call(rbind, time_list))
 # Reshape into long format
 long_df <- melt(time_df, id.vars = c("uid", "time_units", "num_genes"))
 # Convert time to minutes
@@ -48,11 +46,15 @@ long_df$value <- round((long_df$value*(1/60)*(1/60)), digits = 2)
 long_df$time_units <- "hours"
 # Add fancy labels for facets
 long_df$var_label <- factor(long_df$variable, 
-                                 levels = c("likelihood_mapping", "scfs", "ntlt", "delta_plot", "q_residuals", "fast_tiger", "Cunningham_test", "tree_proportion", "total_time"), 
-                                 ordered = TRUE, 
-                                 labels = c("Likelihood\nmapping", "sCF", "Network\nTreelikeness\nTest", "Delta\nplot", "Q-Residual", "Fast\nTIGER", "Cunningham\nmetric", "Tree\nproportion", "Total\nruntime") )
+                            levels = c("likelihood_mapping", "scfs", "ntlt", "delta_plot", "q_residuals", "fast_tiger", "Cunningham_test", "tree_proportion", "total_time"), 
+                            ordered = TRUE, 
+                            labels = c("Likelihood\nmapping", "sCF", "Network\nTreelikeness\nTest", "Delta\nplot", "Q-Residual", "Fast\nTIGER", "Cunningham\nmetric", "Tree\nproportion", "Total\nruntime") )
+
+## 250 genes timing: pretty box plot ##
+# Reduce the dataframe to only rows for runs with 250 genes
+plot_df <- long_df[long_df$num_genes == 250,]
 # Create a discrete box plot where the tests are the x axis and time in minutes is on the y axis
-p <- ggplot(data = long_df, aes(x = var_label, y = value)) +
+p <- ggplot(data = plot_df, aes(x = var_label, y = value)) +
   geom_boxplot(outlier.alpha = 0.6) +
   scale_x_discrete(name = "Test statistic") +
   scale_y_continuous(name = "Time to run test statistic (hours)", breaks = seq(0, 28, 4), labels = seq(0, 28, 4), minor_breaks = seq(0,28,2), limits = c(0,26)) +
@@ -64,6 +66,43 @@ p <- ggplot(data = long_df, aes(x = var_label, y = value)) +
 # Save plot
 p_name <- paste0(plot_directory, "exp2_timing_boxplots_313obs_250genes.pdf")
 ggsave(filename = p_name, plot = p, device = "pdf", height = 8, width = 8, units = "in")
+
+## 250 genes and 1000 timing: pretty box plot ##
+plot_df <- long_df
+# Create a discrete box plot where the tests are the x axis and time in minutes is on the y axis
+p <- ggplot(data = long_df, aes(x = var_label, y = value, fill = as.factor(num_genes))) +
+  geom_boxplot(outlier.alpha = 0.4) +
+  scale_x_discrete(name = "Test statistic") +
+  scale_y_continuous(name = "Time to run test statistic (hours)", breaks = seq(0, 160, 20), labels = seq(0, 160, 20), minor_breaks = seq(0,160,5), limits = c(0,140)) +
+  guides(fill = guide_legend(title = "Number\nof genes")) +
+  labs(title = "Experiment 2: Simulations with introgression run times") +
+  scale_fill_manual(values = c("#7fcdbb", "#2c7fb8")) +
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5, size = 17),
+        axis.title.x = element_text(size = 14), axis.text.x = element_text(size = 11),
+        axis.title.y = element_text(size = 14), axis.text.y = element_text(size = 12))
+# Save plot
+p_name <- paste0(plot_directory, "exp2_timing_boxplots_comparison_fills.pdf")
+ggsave(filename = p_name, plot = p, device = "pdf", height = 8, width = 8, units = "in")
+
+## Faceted 250 genes and 1000 timing: pretty box plot ##
+plot_df <- long_df
+# Create a facet labeller
+facet_names <- c(`250` = "250 genes (each 4000 bp long)", `1000` = "1000 genes (each 1000bp long")
+# Create a discrete box plot where the tests are the x axis and time in minutes is on the y axis
+p <- ggplot(data = long_df, aes(x = var_label, y = value)) +
+  geom_boxplot(outlier.alpha = 0.4) +
+  facet_wrap(~num_genes, scale = "free_y", labeller = as_labeller(facet_names)) +
+  scale_x_discrete(name = "Test statistic") +
+  scale_y_continuous(name = "Time to run test statistic (hours)") +
+  labs(title = "Experiment 2: Simulations with introgression run times") +
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5, size = 17),
+        axis.title.x = element_text(size = 12), axis.text.x = element_text(size = 10),
+        axis.title.y = element_text(size = 12), axis.text.y = element_text(size = 10))
+# Save plot
+p_name <- paste0(plot_directory, "exp2_timing_boxplots_comparison_facets.pdf")
+ggsave(filename = p_name, plot = p, device = "pdf", height = 8, width = 13, units = "in")
 
 
 

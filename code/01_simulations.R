03da2a2ee3d1d59920a56d0e20c283200249e04a
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-06-15 15:31:43 +1000

    Add control flags to repeat experiment 2

--- code/01_simulations.R
+++ code/01_simulations.R
@@ -24,6 +24,8 @@
 # r_vec                           <- Values of introgression (we chose from 0 to 1 in intervals of 0.05)
 # alisim_gene_models              <- Model of sequence evolution for Alisim 
 # alisim_gene_tree_length         <- Gene-specific tree length for Alisim
+# run.experiment.1                <- Control flag for experiment 1 (TRUE to run code to generate simulations for Experiment 1)
+# run.experiment.2                <- Control flag for experiment 2 (TRUE to run code to generate simulations for Experiment 2)
 
 run_location = "soma"
 if (run_location == "local"){
@@ -50,9 +52,10 @@ tree_depth_coalescent <- c(0.1, 1, 10, 100) # where bounds for coalescent tree d
 number_gene_trees <- 250
 r_vec <- seq(0, 0.5, 0.05)
 alisim_gene_models <- "JC"
-alisim_gene_tree_length <- NA
+alisim_gene_tree_length <- 0.1
 
-
+run.experiment.1 <- FALSE
+run.experiment.2 <- TRUE
 
 #### 2. Open packages ####
 library(ape)
@@ -82,48 +85,49 @@ number_of_replicates <- 1:num_reps
 #     Length of alignment for each tree is total alignment length divided by the number of trees
 #     Number of taxa varies from 10 to 1000
 # Simulate DNA along each tree with Alisim, using the topology-unlinked partition model
-
-# Create folder to store results of this experiment, if it doesn't already exist
-exp1_dir <- paste0(local_directory, "exp_1/")
-if(!file.exists(exp1_dir)){dir.create(exp1_dir)}
-
-# Create matrix with parameters for generating each simulated alignment
-exp1_params <- expand.grid("num_reps" = number_of_replicates, "num_taxa" = number_of_taxa, "num_trees" = number_of_trees, "tree_depth" = tree_depth_random)
-# Add a unique identifier (uid) of the form: experiment_`number of trees`_`number of taxa`_`replicate number`_`tree_depth`
-exp1_params$uid <- paste0("exp1_",sprintf("%05d", exp1_params$num_trees), "_", sprintf("%04d", exp1_params$num_taxa), "_",
-                          sprintf("%03d", exp1_params$num_reps), "_", exp1_params$tree_depth)
-# Add parameters for Alisim
-exp1_params$alisim_gene_models <- alisim_gene_models
-exp1_params$alisim_gene_tree_length <- alisim_gene_tree_length
-# Add other parameters
-exp1_params$total_alignment_length <- total_alignment_length
-exp1_params$sequence_type <- sequence_type
-# Add names for the tree file, partition file and output alignment file for each simulated alignment
-exp1_params$tree_file <- paste0(exp1_params$uid, "_random_trees.phy")
-exp1_params$partition_file <- paste0(exp1_params$uid, "_partitions.nex")
-exp1_params$output_alignment_file <- paste0(exp1_params$uid, "_output_alignment")
-
-# Write exp1_params dataframe to file as a csv
-exp1_df_path <- paste0(local_directory, "exp1_parameters.csv")
-write.csv(exp1_params, file = exp1_df_path, row.names = TRUE)
-
-# Iterate through each row in the parameters dataframe
-# Run all reps:
-#   lapply(1:nrow(exp1_params), random.trees.generate.alignment, output_directory = exp1_dir, iqtree2_path = iqtree2_path, experiment_params = exp1_params)
-# Run single rep:
-# lapply(1, random.trees.generate.alignment, output_directory = exp1_dir, iqtree2_path = iqtree2_path, experiment_params = exp1_params)
-
-if (number_parallel_threads == 1){
-  exp1_op_list <- lapply(1:nrow(exp1_params), random.trees.generate.alignment, output_directory = exp1_dir, iqtree2_path = iqtree2_path, experiment_params = exp1_params)
-} else {
-  exp1_op_list <- mclapply(1:nrow(exp1_params), random.trees.generate.alignment, output_directory = exp1_dir, iqtree2_path = iqtree2_path, experiment_params = exp1_params,
-                           mc.cores = number_parallel_threads)
+if (run.experiment.1 == TRUE){
+  # Create folder to store results of this experiment, if it doesn't already exist
+  exp1_dir <- paste0(local_directory, "exp_1/")
+  if(!file.exists(exp1_dir)){dir.create(exp1_dir)}
+  
+  # Create matrix with parameters for generating each simulated alignment
+  exp1_params <- expand.grid("num_reps" = number_of_replicates, "num_taxa" = number_of_taxa, "num_trees" = number_of_trees, "tree_depth" = tree_depth_random)
+  # Add a unique identifier (uid) of the form: experiment_`number of trees`_`number of taxa`_`replicate number`_`tree_depth`
+  exp1_params$uid <- paste0("exp1_",sprintf("%05d", exp1_params$num_trees), "_", sprintf("%04d", exp1_params$num_taxa), "_",
+                            sprintf("%03d", exp1_params$num_reps), "_", exp1_params$tree_depth)
+  # Add parameters for Alisim
+  exp1_params$alisim_gene_models <- alisim_gene_models
+  exp1_params$alisim_gene_tree_length <- alisim_gene_tree_length
+  # Add other parameters
+  exp1_params$total_alignment_length <- total_alignment_length
+  exp1_params$sequence_type <- sequence_type
+  # Add names for the tree file, partition file and output alignment file for each simulated alignment
+  exp1_params$tree_file <- paste0(exp1_params$uid, "_random_trees.phy")
+  exp1_params$partition_file <- paste0(exp1_params$uid, "_partitions.nex")
+  exp1_params$output_alignment_file <- paste0(exp1_params$uid, "_output_alignment")
+  
+  # Write exp1_params dataframe to file as a csv
+  exp1_df_path <- paste0(local_directory, "exp1_parameters.csv")
+  write.csv(exp1_params, file = exp1_df_path, row.names = TRUE)
+  
+  # Iterate through each row in the parameters dataframe
+  # Run all reps:
+  #   lapply(1:nrow(exp1_params), random.trees.generate.alignment, output_directory = exp1_dir, iqtree2_path = iqtree2_path, experiment_params = exp1_params)
+  # Run single rep:
+  # lapply(1, random.trees.generate.alignment, output_directory = exp1_dir, iqtree2_path = iqtree2_path, experiment_params = exp1_params)
+  
+  if (number_parallel_threads == 1){
+    exp1_op_list <- lapply(1:nrow(exp1_params), random.trees.generate.alignment, output_directory = exp1_dir, iqtree2_path = iqtree2_path, experiment_params = exp1_params)
+  } else {
+    exp1_op_list <- mclapply(1:nrow(exp1_params), random.trees.generate.alignment, output_directory = exp1_dir, iqtree2_path = iqtree2_path, experiment_params = exp1_params,
+                             mc.cores = number_parallel_threads)
+  }
+  
+  # Change output file names from list to dataframe
+  exp1_op_df <- as.data.frame(do.call(rbind, exp1_op_list))
+  exp1_op_df_path <- paste0(local_directory, "exp1_file_output_paths.csv")
+  write.csv(exp1_op_df, file = exp1_op_df_path, row.names = TRUE)
 }
-
-# Change output file names from list to dataframe
-exp1_op_df <- as.data.frame(do.call(rbind, exp1_op_list))
-exp1_op_df_path <- paste0(local_directory, "exp1_file_output_paths.csv")
-write.csv(exp1_op_df, file = exp1_op_df_path, row.names = TRUE)
 
 
 
@@ -133,57 +137,60 @@ write.csv(exp1_op_df, file = exp1_op_df_path, row.names = TRUE)
 #   - Simulate gene trees for tree containing a single introgression event in ms. Vary proportion of introgressed DNA from 0 to 1
 #   - Simulate DNA along each tree with Alisim
 #   - Concatenate alignments
-
-# Create folder to store results of this experiment, if it doesn't already exist
-exp2_dir <- paste0(local_directory, "exp_2/")
-if (dir.exists(exp2_dir) == FALSE){dir.create(exp2_dir)}
-
-exp2_params <- expand.grid("num_reps" = number_of_replicates, "num_taxa" = number_of_taxa, "num_trees" = number_gene_trees, 
-                           "tree_depth_coalescent" = tree_depth_coalescent, "recombination_value" = r_vec, "recombination_type" = c("Ancient","Recent"))
-# Add a unique identifier (uid) of the form: experiment_`number of trees`_`number of taxa`_`replicate number`_`tree_depth`_`recombination proportion`_`introgression event type`
-exp2_params$uid <- paste0("exp2_",sprintf("%05d", exp2_params$num_trees), "_", sprintf("%04d", exp2_params$num_taxa), "_",
-                          sprintf("%03d", exp2_params$num_reps), "_", exp2_params$tree_depth, "_", exp2_params$recombination_value,
-                          "_", exp2_params$recombination_type)
-# Add parameters for Alisim
-exp2_params$alisim_gene_models <- alisim_gene_models
-exp2_params$alisim_gene_tree_length <- alisim_gene_tree_length
-# Add other parameters
-exp2_params$tree_depth_subs_per_sites <- 0.5
-exp2_params$total_alignment_length <- number_gene_trees * gene_length
-exp2_params$sequence_type <- sequence_type
-# Add name for the partition file and output alignment file for each simulated alignment
-exp2_params$partition_file <- paste0(exp2_params$uid, "_partitions.nex")
-exp2_params$output_alignment_file <- paste0(exp2_params$uid, "_output_alignment")
-
-# Remove any rows that have 5 taxa and an ancient introgression event
-# Due to the way ancient introgression events are structured (take place at time where four taxa exist, between two non-sister taxa), a recent and an ancient
-#     introgression event will be identical for a tree with 5 taxa
-remove_rows <- which(exp2_params$num_taxa == 5 & exp2_params$recombination_type == "Ancient")
-keep_rows <- setdiff(1:nrow(exp2_params), remove_rows)
-exp2_params <- exp2_params[keep_rows, ]
-
-
-# Write exp2_params dataframe to file as a csv
-exp2_df_path <- paste0(local_directory, "exp2_parameters.csv")
-write.csv(exp2_params, file = exp2_df_path, row.names = TRUE)
-
-# Iterate through each row in the parameters dataframe and generate an alignment for each set of parameters
-# Run all reps: 
-#   lapply(1:nrow(exp2_params), ms.generate.alignment, output_directory = exp2_dir, ms_path = ms_path, iqtree2_path = iqtree2_path, experiment_params_df = exp2_params)
-# Run single rep:
-#   lapply(1, ms.generate.alignment, output_directory = exp2_dir, ms_path = ms_path, iqtree2_path = iqtree2_path, experiment_params_df = exp2_params, select.sister = FALSE)
-if (number_parallel_threads == 1){
-  exp2_op_list<- lapply(1:nrow(exp2_params), ms.generate.alignment, output_directory = exp2_dir, ms_path = ms_path, iqtree2_path = iqtree2_path,
-                        experiment_params_df = exp2_params, select.sister = FALSE, scale.random.tree = FALSE, scale.gene.trees = TRUE)
-} else {
-  exp2_op_list <- mclapply(1:nrow(exp2_params), ms.generate.alignment, output_directory = exp2_dir, ms_path = ms_path, iqtree2_path = iqtree2_path,
-                           experiment_params_df = exp2_params, select.sister = FALSE, scale.random.tree = FALSE, scale.gene.trees = TRUE, 
-                           mc.cores = number_parallel_threads)
+if (run.experiment.2 == TRUE){
+  # Create folder to store results of this experiment, if it doesn't already exist
+  exp2_dir <- paste0(local_directory, "exp_2/")
+  if (dir.exists(exp2_dir) == FALSE){dir.create(exp2_dir)}
+  
+  exp2_params <- expand.grid("num_reps" = number_of_replicates, "num_taxa" = number_of_taxa, "num_trees" = number_gene_trees, 
+                             "tree_depth_coalescent" = tree_depth_coalescent, "recombination_value" = r_vec, "recombination_type" = c("Ancient","Recent"))
+  # Add a unique identifier (uid) of the form: experiment_`number of trees`_`number of taxa`_`replicate number`_`tree_depth`_`recombination proportion`_`introgression event type`
+  exp2_params$uid <- paste0("exp2_",sprintf("%05d", exp2_params$num_trees), "_", sprintf("%04d", exp2_params$num_taxa), "_",
+                            sprintf("%03d", exp2_params$num_reps), "_", exp2_params$tree_depth, "_", exp2_params$recombination_value,
+                            "_", exp2_params$recombination_type)
+  # Add parameters for Alisim
+  exp2_params$alisim_gene_models <- alisim_gene_models
+  exp2_params$alisim_gene_tree_length <- alisim_gene_tree_length
+  # Add other parameters
+  exp2_params$tree_depth_subs_per_sites <- 0.5
+  exp2_params$total_alignment_length <- number_gene_trees * gene_length
+  exp2_params$sequence_type <- sequence_type
+  # Add name for the partition file and output alignment file for each simulated alignment
+  exp2_params$partition_file <- paste0(exp2_params$uid, "_partitions.nex")
+  exp2_params$output_alignment_file <- paste0(exp2_params$uid, "_output_alignment")
+  
+  # Remove any rows that have 5 taxa and an ancient introgression event
+  # Due to the way ancient introgression events are structured (take place at time where four taxa exist, between two non-sister taxa), a recent and an ancient
+  #     introgression event will be identical for a tree with 5 taxa
+  remove_rows <- which(exp2_params$num_taxa == 5 & exp2_params$recombination_type == "Ancient")
+  keep_rows <- setdiff(1:nrow(exp2_params), remove_rows)
+  exp2_params <- exp2_params[keep_rows, ]
+  
+  
+  # Write exp2_params dataframe to file as a csv
+  exp2_df_path <- paste0(local_directory, "exp2_parameters.csv")
+  write.csv(exp2_params, file = exp2_df_path, row.names = TRUE)
+  
+  # Iterate through each row in the parameters dataframe and generate an alignment for each set of parameters
+  # Run all reps: 
+  #   lapply(1:nrow(exp2_params), ms.generate.alignment, output_directory = exp2_dir, ms_path = ms_path, iqtree2_path = iqtree2_path, experiment_params_df = exp2_params)
+  # Run single rep:
+  #   lapply(1, ms.generate.alignment, output_directory = exp2_dir, ms_path = ms_path, iqtree2_path = iqtree2_path, experiment_params_df = exp2_params, select.sister = FALSE)
+  if (number_parallel_threads == 1){
+    exp2_op_list<- lapply(1:nrow(exp2_params), ms.generate.alignment, output_directory = exp2_dir, ms_path = ms_path, iqtree2_path = iqtree2_path,
+                          experiment_params_df = exp2_params, select.sister = FALSE, scale.random.tree = FALSE, scale.gene.trees = TRUE)
+  } else {
+    exp2_op_list <- mclapply(1:nrow(exp2_params), ms.generate.alignment, output_directory = exp2_dir, ms_path = ms_path, iqtree2_path = iqtree2_path,
+                             experiment_params_df = exp2_params, select.sister = FALSE, scale.random.tree = FALSE, scale.gene.trees = TRUE, 
+                             mc.cores = number_parallel_threads)
+  }
+  
+  # Change output file names from list to dataframe
+  exp2_op_df <- as.data.frame(do.call(rbind, exp2_op_list))
+  exp2_op_df_path <- paste0(local_directory, "exp2_file_output_paths.csv")
+  write.csv(exp2_op_df, file = exp2_op_df_path, row.names = TRUE)
 }
 
-# Change output file names from list to dataframe
-exp2_op_df <- as.data.frame(do.call(rbind, exp2_op_list))
-exp2_op_df_path <- paste0(local_directory, "exp2_file_output_paths.csv")
-write.csv(exp2_op_df, file = exp2_op_df_path, row.names = TRUE)
+
 
 

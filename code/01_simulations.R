2df8ef19b517e14a4a818f465856284e154e5b85
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-07-28 14:25:03 +1000

    Add parameters and file paths for experiment 3 (Yule trees with speciation).

--- code/01_simulations.R
+++ code/01_simulations.R
@@ -7,14 +7,14 @@
 
 
 #### 1. Set parameters ####
-# local_directory                 <- Directory where alignments will be saved/treelikeness metrics will be run.
+# output_directory                 <- Directory where alignments will be saved/treelikeness metrics will be run.
 # repo_directory                  <- Location of caitlinch/treelikeness-metrics github repository (for access to functions).
 # ms_path                         <- Path to ms executable 
 
 # iqtree2_path                    <- Path to IQ-Tree2 executable (version 2.2-beta or later to ensure Alisim is included). 
 # number_parallel_threads         <- Number of threads to run simultaneously in mclapply when generating alignments
 
-# total_alignment_length          <- Total length of concatenated alignments in base pairs (we chose 10000) for the random tree analyses.
+# total_alignment_length          <- Total length of concatenated alignments in base pairs (we chose 10000) for the random tree analyses (experiment 1).
 # gene_length                     <- Length of each gene generated in ms. Total alignment length will be length of each gene multiplied by number of gene trees. 
 #                                     For Total Alignment Length = 10000, use 100 gene trees of 100 bp each.
 # sequence_type                   <- Sequence type for simulation (we chose "DNA").
@@ -22,6 +22,7 @@
 # num_reps                        <- Number of replicates to run for each set of simulation conditions (we chose 10). Must be >= 1.
 # tree_depth_random_sims          <- One or more values for the tree length of randomly generated trees (in substitutions per site).
 # tree_depth_coalescent_sims      <- One or more values for the tree length of coalescent trees (in coalescent units).
+# speciation_rates                <- One of more values for the speciation rate for the introgression simulations (experiment 3)
 # number_gene_trees               <- Number of gene trees to generate for coalescent simulations
 # r_vec                           <- Values of introgression (we chose from 0 to 1 in intervals of 0.05)
 # alisim_gene_models              <- Model of sequence evolution for Alisim 
@@ -29,17 +30,17 @@
 # conversion_depth_subs_per_site     <- Tree depth for ML tree - used to convert depth of ms gene trees from coalescent units to substitutions per site
 
 # run.experiment.1                <- Control flag for experiment 1 (TRUE to run code to generate simulations for Experiment 1)
-# run.experiment.2                <- Control flag for experiment 2 (TRUE to run code to generate simulations for Experiment 2)
+# run.experiment.3                <- Control flag for experiment 3 (TRUE to run code to generate simulations for Experiment 3)
 
 run_location = "soma"
 if (run_location == "local"){
-  local_directory         <- "/Users/caitlincherryh/Documents/C2_TreelikenessMetrics/"
+  output_directory        <- "/Users/caitlincherryh/Documents/C2_TreelikenessMetrics/"
   repo_directory          <- "/Users/caitlincherryh/Documents/Repositories/treelikeness-metrics/"
   ms_path                 <- "ms"
   iqtree2_path            <- "iqtree2"
   number_parallel_threads <- 1
 } else if (run_location == "soma"){
-  local_directory         <- "/data/caitlin/treelikeness_metrics/"
+  output_directory         <- "/data/caitlin/treelikeness_metrics/"
   repo_directory          <- "/data/caitlin/treelikeness_metrics/"
   ms_path                 <- "/data/caitlin/executables/msdir/ms"
   iqtree2_path            <- "/data/caitlin/linux_executables/iqtree-2.2.0-Linux/bin/iqtree2"
@@ -47,20 +48,21 @@ if (run_location == "local"){
 }
 
 total_alignment_length          <- 10000
-gene_length                     <- 1000
+gene_length                     <- 500
 sequence_type                   <- "DNA"
 taxa_vec                        <- c(5,10,20,50,100)
 num_reps                        <- 10
 tree_depth_random_sims          <- c(0.01, 0.1, 1)
 tree_depth_coalescent_sims      <- c(0.1, 1, 10, 100) # where bounds for coalescent tree depth are 0.1 (minimum) and 100 (maximum) in coalescent units
-number_gene_trees               <- 250
+speciation_rates                <- c(0.1, 1) # for generating Yule tree for the introgression experiments (experiment 3)
+number_gene_trees               <- 200
 r_vec                           <- seq(0, 0.5, 0.05)
 alisim_gene_models              <- "JC"
 alisim_gene_tree_length         <- NA
 conversion_depth_subs_per_site  <- 0.1
 
 run.experiment.1 <- FALSE
-run.experiment.2 <- TRUE
+run.experiment.3 <- TRUE
 
 
 
@@ -94,7 +96,7 @@ number_of_replicates <- 1:num_reps
 # Simulate DNA along each tree with Alisim, using the topology-unlinked partition model
 if (run.experiment.1 == TRUE){
   # Create folder to store results of this experiment, if it doesn't already exist
-  exp1_dir <- paste0(local_directory, "exp_1/")
+  exp1_dir <- paste0(output_directory, "exp_1/")
   if(!file.exists(exp1_dir)){dir.create(exp1_dir)}
   
   # Create matrix with parameters for generating each simulated alignment
@@ -114,14 +116,14 @@ if (run.experiment.1 == TRUE){
   exp1_params$output_alignment_file <- paste0(exp1_params$uid, "_output_alignment")
   
   # Write exp1_params dataframe to file as a csv
-  exp1_df_path <- paste0(local_directory, "exp1_parameters.csv")
+  exp1_df_path <- paste0(output_directory, "exp1_parameters.csv")
   write.csv(exp1_params, file = exp1_df_path, row.names = TRUE)
   
   # Iterate through each row in the parameters dataframe
   # Run all reps:
   #   lapply(1:nrow(exp1_params), random.trees.generate.alignment, output_directory = exp1_dir, iqtree2_path = iqtree2_path, experiment_params = exp1_params)
   # Run single rep:
-  # lapply(1, random.trees.generate.alignment, output_directory = exp1_dir, iqtree2_path = iqtree2_path, experiment_params = exp1_params)
+  #   lapply(1, random.trees.generate.alignment, output_directory = exp1_dir, iqtree2_path = iqtree2_path, experiment_params = exp1_params)
   
   if (number_parallel_threads == 1){
     exp1_op_list <- lapply(1:nrow(exp1_params), random.trees.generate.alignment, output_directory = exp1_dir, iqtree2_path = iqtree2_path, experiment_params = exp1_params)
@@ -132,7 +134,7 @@ if (run.experiment.1 == TRUE){
   
   # Change output file names from list to dataframe
   exp1_op_df <- as.data.frame(do.call(rbind, exp1_op_list))
-  exp1_op_df_path <- paste0(local_directory, "exp1_file_output_paths.csv")
+  exp1_op_df_path <- paste0(output_directory, "exp1_file_output_paths.csv")
   write.csv(exp1_op_df, file = exp1_op_df_path, row.names = TRUE)
 }
 
@@ -144,17 +146,18 @@ if (run.experiment.1 == TRUE){
 #   - Simulate gene trees for tree containing a single introgression event in ms. Vary proportion of introgressed DNA from 0 to 1
 #   - Simulate DNA along each tree with Alisim
 #   - Concatenate alignments
-if (run.experiment.2 == TRUE){
+if (run.experiment.3 == TRUE){
   # Create folder to store results of this experiment, if it doesn't already exist
-  exp3_dir <- paste0(local_directory, "exp_2/")
+  exp3_dir <- paste0(output_directory, "exp_3/")
   if (dir.exists(exp3_dir) == FALSE){dir.create(exp3_dir)}
   
   exp3_params <- expand.grid("num_reps" = number_of_replicates, "num_taxa" = number_of_taxa, "num_trees" = number_gene_trees, 
-                             "tree_depth_coalescent" = tree_depth_coalescent_sims, "recombination_value" = r_vec, "recombination_type" = c("Ancient","Recent"))
+                             "tree_depth_coalescent" = tree_depth_coalescent_sims, "recombination_value" = r_vec, "recombination_type" = c("Ancient","Recent"),
+                             "speciation_rate" = speciation_rates)
   # Add a unique identifier (uid) of the form: experiment_`number of trees`_`number of taxa`_`replicate number`_`tree_depth`_`recombination proportion`_`introgression event type`
   exp3_params$uid <- paste0("exp3_",sprintf("%05d", exp3_params$num_trees), "_", sprintf("%04d", exp3_params$num_taxa), "_",
                             sprintf("%03d", exp3_params$num_reps), "_", exp3_params$tree_depth, "_", exp3_params$recombination_value,
-                            "_", exp3_params$recombination_type)
+                            "_", exp3_params$recombination_type, "_", exp3_params$speciation_rate)
   # Add parameters for Alisim
   exp3_params$alisim_gene_models <- alisim_gene_models
   exp3_params$alisim_gene_tree_length <- alisim_gene_tree_length
@@ -175,7 +178,7 @@ if (run.experiment.2 == TRUE){
   
   
   # Write exp3_params dataframe to file as a csv
-  exp3_df_path <- paste0(local_directory, "exp3_parameters.csv")
+  exp3_df_path <- paste0(output_directory, "exp3_parameters.csv")
   write.csv(exp3_params, file = exp3_df_path, row.names = TRUE)
   
   # Iterate through each row in the parameters dataframe and generate an alignment for each set of parameters
@@ -194,7 +197,7 @@ if (run.experiment.2 == TRUE){
   
   # Change output file names from list to dataframe
   exp3_op_df <- as.data.frame(do.call(rbind, exp3_op_list))
-  exp3_op_df_path <- paste0(local_directory, "exp3_file_output_paths.csv")
+  exp3_op_df_path <- paste0(output_directory, "exp3_file_output_paths.csv")
   write.csv(exp3_op_df, file = exp3_op_df_path, row.names = TRUE)
 }
 

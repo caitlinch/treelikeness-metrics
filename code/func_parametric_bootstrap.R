ddaaa0c213d6f64fe9b8b456f58ee4a1b1caec69
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-08-17 15:57:14 +1000

    New script for parametric bootstrap functions.

---
+++ code/func_parametric_bootstrap.R
@@ -0,0 +1,15 @@
+# caitlinch/treelikeness_metrics/code/func_parametric_bootstraps.R
+# Caitlin Cherryh 2023
+
+# This file contains functions to generate parametric bootstraps from an empirical alignment
+# Some functions require IQ-Tree2 (2.2-beta or above)
+
+#### Parametric bootstrap wrapper ####
+
+
+
+
+#### Generate alignment ####
+
+
+

06f851d500938f3b30bdd729d43672107d5a9c60
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-11-30 16:56:33 +1100

    Complete gene treelikeness plots.

--- code/05_empirical_figures.R
+++ code/05_empirical_figures.R
@@ -6,10 +6,8 @@
 
 #### 1. Set parameters ####
 ## Directories
-# data_directory      <- Directory containing output from gene treelikeness metrics and gene parametric bootstraps
 # figure_directory    <- Directory for saving figures
 # repo_directory      <- Location of caitlinch/treelikeness-metrics github repository (for access to functions)
-
 
 # Directories
 repo_directory <- "/Users/caitlincherryh/Documents/Repositories/treelikeness-metrics/"
@@ -19,13 +17,9 @@ plot_directory <- "/Users/caitlincherryh/Documents/C2_TreelikenessMetrics/06_plo
 
 #### 2. Prepare analyses ####
 # Open packages
-library(dplyr)
 library(reshape2)
 library(ggplot2)
-library(ggVennDiagram)
-library(ggvenn)
 library(patchwork)
-library(viridisLite)
 
 # Open empirical gene p-value file
 all_op_files <- paste0(paste0(repo_directory, "output/"), list.files(paste0(repo_directory, "output/")))
@@ -54,19 +48,25 @@ ts_df$p_value_significance[p_df$value > 0.05] <- "Not significant"
 # Create labels for plotting
 ts_df$dataset_label <- factor(ts_df$dataset,
                               levels = unique(ts_df$dataset),
-                              labels = c("Original dataset", "Orthology-enriched dataset"),
+                              labels = c("Original dataset", "Orthology-enriched\ndataset"),
                               ordered = TRUE)
 ts_df$variable_label <- factor(ts_df$variable,
                                levels = c("tree_proportion_alignment_value", "sCF_mean_alignment_value", "mean_delta_plot_alignment_value"),
                                labels = c("Tree proportion", "Mean sCF", "Mean delta plot"),
                                ordered = TRUE)
 # Plot histogram for each test statistic, faceted by test statistic and dataset
-ggplot(data = ts_df, aes(x = value)) + 
-  geom_histogram(bins = 40) +
-  facet_grid(variable_label ~ dataset_label, scale = "fixed") +
-  xlab("Test statistic value") +
+hist <- ggplot(data = ts_df, aes(x = value)) + 
+  geom_histogram(binwidth = 0.05) +
+  facet_grid(dataset_label ~ variable_label, scale = "fixed") +
   scale_y_continuous(name = "Frequency") +
-  theme_bw()
+  scale_x_continuous(name = "Test statistic value", limits = c(0, 1), breaks = seq(0, 1, 0.2), labels = seq(0, 1, 0.2), minor_breaks = seq(0, 1, 0.1)) +
+  theme_bw() +
+  theme(axis.title.x = element_text(size = 16, margin = margin(t = 10, r = 0, b = 10, l = 0, unit = "pt")),
+        axis.text.x = element_text(size = 14, angle = 45, hjust = 0.5, vjust = 0.5), 
+        axis.title.y = element_text(size = 16, margin = margin(t = 0, r = 10, b = 0, l = 10, unit = "pt")), 
+        axis.text.y = element_text(size = 14),
+        strip.text.x = element_text(size = 13, margin = margin(t = 5, r = 0, b = 5, l = 0, unit = "pt")),
+        strip.text.y = element_text(size = 13, margin = margin(t = 0, r = 5, b = 0, l = 5, unit = "pt")))
 
 
 
@@ -85,12 +85,18 @@ p_df$variable_label <- factor(p_df$variable,
                               labels = c("Tree proportion", "Mean sCF", "Mean delta plot"),
                               ordered = TRUE)
 # Plot histogram for each p-value, faceted by test statistic and dataset
-ggplot(data = p_df, aes(x = value)) + 
+p_hist <- ggplot(data = p_df, aes(x = value)) + 
   geom_histogram(bins = 20) +
   facet_grid(variable_label ~ dataset_label, scale = "fixed") +
   scale_x_continuous(name = "p-value", breaks = seq(0, 1, 0.2), labels = seq(0, 1, 0.2), minor_breaks = seq(0, 1, 0.1)) +
   scale_y_continuous(name = "Frequency", breaks = seq(0, 120, 30), labels = seq(0, 120, 30), minor_breaks = seq(0, 120, 15), limits = c(0, 120)) +
-  theme_bw()
+  theme_bw() +
+  theme(axis.title.x = element_text(size = 16, margin = margin(t = 10, r = 0, b = 10, l = 0, unit = "pt")),
+        axis.text.x = element_text(size = 14, angle = 45, hjust = 0.5, vjust = 0.5), 
+        axis.title.y = element_text(size = 16, margin = margin(t = 0, r = 10, b = 0, l = 10, unit = "pt")), 
+        axis.text.y = element_text(size = 14),
+        strip.text.x = element_text(size = 16, margin = margin(t = 5, r = 0, b = 5, l = 0, unit = "pt")),
+        strip.text.y = element_text(size = 16, margin = margin(t = 0, r = 5, b = 0, l = 5, unit = "pt")))
 
 
 
@@ -99,34 +105,61 @@ ggplot(data = p_df, aes(x = value)) +
 og_p_df <- p_df[which(p_df$dataset == "WEA17"), ]
 filtered_p_df <- p_df[which(p_df$dataset == "WEA17F"), ]
 # Plot original dataset
-ggplot(data = og_p_df, aes(x = variable_label, fill = p_value_significance)) +
+og_p_plot <- ggplot(data = og_p_df, aes(x = variable_label, fill = p_value_significance)) +
   geom_bar() +
-  scale_fill_manual(name = "P-value result", values = c("#67a9cf", "#ef8a62")) +
+  scale_fill_manual(name = "p-value result", values = c("#67a9cf", "#ef8a62")) +
   labs(title = "Original dataset") +
-  scale_x_discrete(name = "") +
+  scale_x_discrete(name = "Test statistic p-values") +
   scale_y_continuous(name = "Count") +
+  guides(fill = "none") +
   theme_bw() +
-  theme(axis.title.x = NULL,
-        axis.text.x = element_text(size = 14, margin = margin(t = 10, r = 0, b = 10, l = 0, unit = "pt")), 
+  theme(axis.title.x = element_text(size = 16, margin = margin(t = 5, r = 0, b = 5, l = 0, unit = "pt")),
+        axis.text.x = element_text(size = 14, margin = margin(t = 5, r = 0, b = 5, l = 0, unit = "pt"), hjust = 1, vjust = 1, angle = 45), 
         axis.title.y = element_text(size = 16, margin = margin(t = 0, r = 10, b = 0, l = 10, unit = "pt")), 
         axis.text.y = element_text(size = 14),
-        legend.title = element_text(size = 16),
-        legend.text = element_text(size = 14),
         plot.title = element_text(size = 20, hjust = 0.5, margin = margin(t = 10, r = 0, b = 10, l = 0, unit = "pt")) )
 # Plot filtered dataset
-ggplot(data = filtered_p_df, aes(x = variable_label, fill = p_value_significance)) +
+filtered_p_plot <- ggplot(data = filtered_p_df, aes(x = variable_label, fill = p_value_significance)) +
   geom_bar() +
-  scale_fill_manual(name = "P-value result", values = c("#67a9cf", "#ef8a62")) +
+  scale_fill_manual(name = "p-value result", values = c("#67a9cf", "#ef8a62")) +
   labs(title = "Orthology-enriched dataset") +
-  scale_x_discrete(name = "") +
+  scale_x_discrete(name = "Test statistic p-values") +
   scale_y_continuous(name = "Count") +
   theme_bw() +
-  theme(axis.title.x = NULL,
-        axis.text.x = element_text(size = 14, margin = margin(t = 10, r = 0, b = 10, l = 0, unit = "pt")), 
+  theme(axis.title.x = element_text(size = 16, margin = margin(t = 5, r = 0, b = 5, l = 0, unit = "pt")),
+        axis.text.x = element_text(size = 14, margin = margin(t = 5, r = 0, b = 5, l = 0, unit = "pt"), hjust = 1, vjust = 1, angle = 45), 
         axis.title.y = element_text(size = 16, margin = margin(t = 0, r = 10, b = 0, l = 10, unit = "pt")), 
         axis.text.y = element_text(size = 14),
         legend.title = element_text(size = 16),
         legend.text = element_text(size = 14),
         plot.title = element_text(size = 20, hjust = 0.5, margin = margin(t = 10, r = 0, b = 10, l = 0, unit = "pt")) )
+
+# Create tables
+table(og_p_df$p_value_significance, og_p_df$variable_label)
+table(filtered_p_df$p_value_significance, filtered_p_df$variable_label)
+
+
+
+#### 6. Assemble plots using patchwork ####
+# Assemble main figure
+quilt <- hist/(og_p_plot + filtered_p_plot) + 
+  plot_annotation(tag_levels = 'a', tag_suffix = ")") &  theme(plot.tag = element_text(size = 25))
+quilt_file <- paste0(plot_directory, "gene_treelikeness_composite")
+ggsave(filename = paste0(quilt_file, ".pdf"), plot = quilt, width = 10, height = 12)
+ggsave(filename = paste0(quilt_file, ".png"), plot = quilt, width = 10, height = 12)
+# Save test statistic histogram
+plot_file <- paste0(plot_directory, "gene_treelikeness_test_statistic_histogram")
+ggsave(filename = paste0(plot_file, ".pdf"), plot = hist)
+ggsave(filename = paste0(plot_file, ".png"), plot = hist)
+# Save p-value histogram
+plot_file <- paste0(plot_directory, "gene_treelikeness_pvalue_histogram")
+ggsave(filename = paste0(plot_file, ".pdf"), plot = p_hist)
+ggsave(filename = paste0(plot_file, ".png"), plot = p_hist)
+# Save stacked box plots of p-values
+quilt2 <- og_p_plot + filtered_p_plot + plot_layout(ncol = 2, nrow = 1) + 
+  plot_annotation(tag_levels = 'a', tag_suffix = ")") &  theme(plot.tag = element_text(size = 25))
+quilt2_file <- paste0(plot_directory, "gene_treelikeness_pvalue_stacked_bar")
+ggsave(filename = paste0(quilt2_file, ".pdf"), plot = quilt2)
+ggsave(filename = paste0(quilt2_file, ".png"), plot = quilt2)
 
 

da08f82f7f4da601dc210653515f502efa96b8cd
caitlinch <24237077+caitlinch@users.noreply.github.com>
2024-01-30 17:21:21 +1100

    Added new violin plot for manuscript.

--- code/05_empirical_figures.R
+++ code/05_empirical_figures.R
@@ -186,16 +186,16 @@ tp_t <- t.test(gene_op_df$tree_proportion_alignment_value[which(gene_op_df$datas
 # F = 2.3656, num df = 116, denom df = 41, p-value = 0.002256
 # Reject null hypothesis that variances are equal (in t.test function, set var.equal to FALSE)
 scf_f <- var.test(gene_op_df$sCF_mean_alignment_value[which(gene_op_df$dataset == "WEA17")],
-                 gene_op_df$sCF_mean_alignment_value[which(gene_op_df$dataset == "WEA17F")], 
-                 alternative = "two.sided")
+                  gene_op_df$sCF_mean_alignment_value[which(gene_op_df$dataset == "WEA17F")], 
+                  alternative = "two.sided")
 ## t-test (compare means)
 # t = 0.52804, df = 111.72, p-value = 0.5985
 # Accept null hypothesis that the true difference in means is equal to 0
 scf_t <- t.test(gene_op_df$sCF_mean_alignment_value[which(gene_op_df$dataset == "WEA17")], 
-               gene_op_df$sCF_mean_alignment_value[which(gene_op_df$dataset == "WEA17F")],
-               alternative = c("two.sided"),
-               paired = FALSE, var.equal = FALSE,
-               conf.level = 0.95)
+                gene_op_df$sCF_mean_alignment_value[which(gene_op_df$dataset == "WEA17F")],
+                alternative = c("two.sided"),
+                paired = FALSE, var.equal = FALSE,
+                conf.level = 0.95)
 
 
 ### For mean delta plot
@@ -209,8 +209,50 @@ dp_f <- var.test(gene_op_df$mean_delta_plot_alignment_value[which(gene_op_df$dat
 # t = 2.9828, df = 157, p-value = 0.003312
 # Reject null hypothesis that the true difference in means is equal to 0
 dp_t <- t.test(gene_op_df$mean_delta_plot_alignment_value[which(gene_op_df$dataset == "WEA17")], 
-                gene_op_df$mean_delta_plot_alignment_value[which(gene_op_df$dataset == "WEA17F")],
-                alternative = c("two.sided"),
-                paired = FALSE, var.equal = TRUE,
-                conf.level = 0.95)
+               gene_op_df$mean_delta_plot_alignment_value[which(gene_op_df$dataset == "WEA17F")],
+               alternative = c("two.sided"),
+               paired = FALSE, var.equal = TRUE,
+               conf.level = 0.95)
+
+
+
+### 8. Plot violin plots for manuscript ####
+# Create long format dataframe for p-values
+violin_df <- melt(gene_op_df,
+                  id.vars = c("dataset", "gene"),
+                  measure.vars = c("tree_proportion_alignment_value", "sCF_mean_alignment_value", "mean_delta_plot_alignment_value"))
+# Create labels for plotting
+violin_df$dataset_label <- factor(violin_df$dataset,
+                                  levels = unique(violin_df$dataset),
+                                  labels = c("Original", "Orthology-enriched"),
+                                  ordered = TRUE)
+violin_df$variable_label <- factor(violin_df$variable,
+                                   levels = c("tree_proportion_alignment_value", "sCF_mean_alignment_value", "mean_delta_plot_alignment_value"),
+                                   labels = c("Tree proportion", "Mean sCF", "Mean delta plot"),
+                                   ordered = TRUE)
+# Plot the bar chart for the test statistics
+violin_plot <- ggplot(data = violin_df, aes(x = variable_label, y = value, fill = dataset_label)) +
+  geom_boxplot() +
+  scale_fill_manual(name = "Dataset", values = c("#d01c8b", "#b8e186")) +
+  scale_y_continuous(name = "Test statistic value", limits = c(0, 1), breaks = seq(0,1,0.2), labels = seq(0,1,0.2), minor_breaks = seq(0,1,0.05)) +
+  scale_x_discrete(name = NULL) +
+  guides(fill = guide_legend(override.aes = list(size = 12))) +
+  theme_bw() +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_text(size = 20, angle = 45, vjust = 1.0, hjust = 1.0, margin = margin(t = 5, r = 0, b = 0, l = 0, unit = "pt")), 
+        axis.title.y = element_text(size = 26, margin = margin(t = 0, r = 20, b = 0, l = 10, unit = "pt"), color = "grey30"), 
+        axis.text.y = element_text(size = 20, margin = margin(t = 0, r = 5, b = 0, l = 0, unit = "pt")),
+        legend.title = element_text(size = 26),
+        legend.text = element_text(size = 24),
+        axis.line = element_line(linewidth = 1), 
+        axis.ticks = element_line(linewidth = 1),
+        panel.grid.major = element_line(linewidth = 1),
+        panel.grid.minor = element_line(linewidth = 0.8),
+        panel.border = element_rect(linewidth = 2, fill = NA))
+# Save the plot
+violin_path <- paste0(plot_directory, "gene_test_statistics_violin")
+ggsave(filename = paste0(violin_path, ".png"), plot = violin_plot)
+ggsave(filename = paste0(violin_path, ".pdf"), plot = violin_plot)
+
+
 

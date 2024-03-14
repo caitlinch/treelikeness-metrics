53c0ae426b53861a18cddf9018684f81f8475d21
caitlinch <24237077+caitlinch@users.noreply.github.com>
2024-03-14 18:26:20 +1100

    Update plot grids.

--- code/05_empirical_figures.R
+++ code/05_empirical_figures.R
@@ -267,7 +267,7 @@ ggsave(filename = paste0(violin_path2, ".pdf"), plot = violin_plot2)
 ### 9. Combine box plots with bar charts for ms ####
 # Save just the plot
 quilt <- violin_plot/(og_p_plot + filtered_p_plot) + 
-  plot_layout(heights = c(2, 1)) +
+  plot_layout(heights = c(1, 1)) +
   plot_annotation(tag_levels = 'a', tag_suffix = ")") &  theme(plot.tag = element_text(size = 25))
 quilt_file <- paste0(plot_directory, "mainfig_gene_treelikeness_composite_boxplot")
 ggsave(filename = paste0(quilt_file, ".pdf"), plot = quilt, width = 10, height = 12)
@@ -275,7 +275,7 @@ ggsave(filename = paste0(quilt_file, ".png"), plot = quilt, width = 10, height =
 # Save plot with significance markers
 violin_plot_sig <- violin_plot + stat_compare_means(method = "t.test", aes(label = ..p.signif..), label.y = c(0.9, 0.73, 0.6), hide.ns = T, size = 10)
 quilt <- violin_plot_sig/(og_p_plot + filtered_p_plot) + 
-  plot_layout(heights = c(2, 1)) +
+  plot_layout(heights = c(1, 1)) +
   plot_annotation(tag_levels = 'a', tag_suffix = ")") &  theme(plot.tag = element_text(size = 25))
 quilt_file <- paste0(plot_directory, "mainfig_gene_treelikeness_composite_boxplot_significance")
 ggsave(filename = paste0(quilt_file, ".pdf"), plot = quilt, width = 10, height = 12)
@@ -283,7 +283,7 @@ ggsave(filename = paste0(quilt_file, ".png"), plot = quilt, width = 10, height =
 # Save plot with p-values
 violin_plot_pval <- violin_plot + stat_compare_means(method = "t.test", aes(label = paste0("p=", ..p.format..)), label.y = c(0.9, 0.73, 0.6), size = 5)
 quilt <- violin_plot_pval/(og_p_plot + filtered_p_plot) + 
-  plot_layout(heights = c(2, 1)) +
+  plot_layout(heights = c(1, 1)) +
   plot_annotation(tag_levels = 'a', tag_suffix = ")") &  theme(plot.tag = element_text(size = 25))
 quilt_file <- paste0(plot_directory, "mainfig_gene_treelikeness_composite_boxplot_pvalues")
 ggsave(filename = paste0(quilt_file, ".pdf"), plot = quilt, width = 10, height = 12)

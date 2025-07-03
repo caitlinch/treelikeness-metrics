985de4308f9cbf693a84dd974bda6b091a9fe708
Caitlin Cherryh <24237077+caitlinch@users.noreply.github.com>
2025-07-03 12:29:18 +1000

    New script to generate alignments for supp B

---
+++ supp_code/suppB_01_simulations.R
@@ -0,0 +1,289 @@
+## Supplementary analysis B: Tree Proportion
+
+# caitlinch/treelikeness-metrics/supp_code/suppB_01_simulations.R
+# Caitlin Cherryh 2025
+
+# This program will simulate alignments with varying levels of treelikeness
+# This program requires IQ-Tree2 (2.2-beta or above) and ms.
+
+
+
+#### 1. Set parameters ####
+## CONTROL PARAMETERS
+# parameter.values  <- TRUE to input simulation parameter values
+# run.experiment.B1 <- TRUE to run code to generate simulations for Exp. B1
+# run.experiment.B2 <- TRUE to run code to generate simulations for Exp. B2
+
+## DIRECTORY PATHS
+# simulation_directory        <- Directory where alignments will be saved/treelikeness metrics will be run.
+# repo_directory              <- Location of caitlinch/treelikeness-metrics github repository (for access to functions).
+# ms_path                     <- Path to ms executable
+# iqtree2_path                <- Path to IQ-Tree2 executable (version 2.2-beta or later to ensure Alisim is included).
+# number_parallel_threads     <- Number of threads to run simultaneously in mclapply when generating alignments
+
+## SIMULATION PARAMETERS
+# total_alignment_length      <- Total length of concatenated alignments in base pairs (we chose 10000) for the random tree analyses (experiment 1).
+# gene_length                 <- Length of each gene generated in ms. Total alignment length will be length of each gene multiplied by number of gene trees.
+#                                 For Total Alignment Length = 10000, use 100 gene trees of 100 bp each.
+# sequence_type               <- Sequence type for simulation (we chose "DNA").
+# taxa_vec                    <- Number of taxa to simulate (we chose 10,20,50,100,200,500, and 1000).
+# num_reps                    <- Number of replicates to run for each set of simulation conditions (we chose 10). Must be >= 1.
+# tree_depth_random_sims      <- One or more values for the tree length of randomly generated trees (in substitutions per site).
+# tree_depth_coalescent_sims  <- One or more values for the tree length of coalescent trees (in coalescent units).
+# speciation_rates            <- One of more values for the speciation rate for the introgression simulations (experiment 3)
+# number_gene_trees           <- Number of gene trees to generate for coalescent simulations
+# r_vec                       <- Values of introgression (we chose from 0 to 1 in intervals of 0.05)
+# alisim_gene_models          <- Model of sequence evolution for Alisim
+# alisim_gene_tree_length     <- Gene-specific tree length for Alisim
+
+
+## CONTROL PARAMETERS
+parameter.values  <- FALSE
+run.experiment.B1 <- TRUE
+run.experiment.B2 <- TRUE
+
+## DIRECTORY PATHS
+run_location = "dayhoff"
+if (run_location == "dayhoff"){
+  repo_directory          <- "/mnt/data/dayhoff/home/u5348329/treelikeness_supp/"
+  simulation_directory    <- "/mnt/data/dayhoff/home/u5348329/treelikeness_supp/suppB/"
+  iqtree2_path            <- "/mnt/data/dayhoff/home/u5348329/treelikeness_supp/software/iqtree-2.2.2.6-Linux/bin/iqtree2"
+  ms_path                 <- "/mnt/data/dayhoff/home/u5348329/treelikeness_supp/software/msdir/ms"
+  number_parallel_threads <- 10
+} else if (run_location == "WSL"){
+  repo_directory          <- ""
+  simulation_directory    <- "suppA/"
+  iqtree2_path            <- "iqtree2"
+  ms_path                 <- NA
+  number_parallel_threads <- 1
+  if (dir.exists(simulation_directory) == FALSE){dir.create(simulation_directory)}
+}
+
+## SIMULATION PARAMETERS
+if (parameter.values == TRUE){
+  total_alignment_length          <- 10000
+  gene_length                     <- 200
+  sequence_type                   <- "DNA"
+  taxa_vec                        <- c(5,10,20,50,100)
+  num_reps                        <- 10
+  tree_depth_random_sims          <- c(0.01, 0.1, 1)
+  tree_age                        <- c(5, 50, 500) # where bounds for coalescent tree depth are in millions of years (see TreeSim doco)
+  speciation_rates                <- c(0.1, 1) # for generating Yule tree for the introgression experiments (experiment 3)
+  number_gene_trees               <- 200
+  r_vec                           <- seq(0, 0.5, 0.05)
+  alisim_gene_models              <- "JC"
+  alisim_gene_tree_length         <- NA
+
+  # Set number of taxa equal to taxa_vec
+  number_of_taxa <- taxa_vec
+  # Create a list of all replicate numbers using the num_reps value
+  number_of_replicates <- 1:num_reps
+}
+
+
+
+#### 2. Prepare packages and functions ####
+# Load packages
+library(ape)
+library(phytools)
+library(parallel)
+
+# Load functions
+source(paste0(repo_directory, "code/func_simulating_alignments.R"))
+
+
+
+#### 4. Generate simulations ####
+## Experiment B1: Random trees ##
+# Generate x random trees with y taxa
+#     Total alignment length = 10,000 bp
+#     Number of trees ranges from 0 to 10,000 (whole number divisors of 10000)
+#     Length of alignment for each tree is total alignment length divided by the number of trees
+#     Number of taxa varies from 10 to 1000
+# Simulate DNA along each tree with Alisim, using the topology-unlinked partition model
+if (run.experiment.B1 == TRUE) {
+  # Create folder to store results of this experiment, if it doesn't already exist
+  expB1_dir <- paste0(simulation_directory, "exp_B1/")
+  if (!file.exists(expB1_dir)) {
+    dir.create(expB1_dir)
+  }
+  # Create (or open if it exists) dataframe of simulation parameters
+  expB1_df_path <- paste0(simulation_directory, "expB1_parameters.csv")
+  if (file.exists(expB1_df_path) == TRUE) {
+    expB1_params <- read.csv(expB1_df_path)
+  } else {
+    # Create matrix with parameters for generating each simulated alignment
+    expB1_params <- expand.grid(
+      "num_reps" = number_of_replicates,
+      "num_taxa" = number_of_taxa,
+      "num_trees" = divisors(total_alignment_length),
+      "tree_depth" = tree_depth_random_sims
+    )
+    # Add a unique identifier (uid) of the form:
+    #   expB1_{num. trees}_{num. taxa}_{rep. num.}_{tree depth}
+    expB1_params$uid <- paste0(
+      "expB1_",
+      sprintf("%05d", expB1_params$num_trees),
+      "_",
+      sprintf("%04d", expB1_params$num_taxa),
+      "_",
+      sprintf("%03d", expB1_params$num_reps),
+      "_",
+      expB1_params$tree_depth
+    )
+    # Add parameters for Alisim
+    expB1_params$alisim_gene_models <- alisim_gene_models
+    expB1_params$alisim_gene_tree_length <- alisim_gene_tree_length
+    # Add other parameters
+    expB1_params$total_alignment_length <- total_alignment_length
+    expB1_params$sequence_type <- sequence_type
+    # Add output file names for simulated alignments
+    expB1_params$tree_file <- paste0(expB1_params$uid, "_random_trees.phy")
+    expB1_params$partition_file <- paste0(expB1_params$uid, "_partitions.nex")
+    expB1_params$output_alignment_file <- paste0(expB1_params$uid, "_output_alignment")
+    # Write expB1_params dataframe to file as a csv
+    write.csv(expB1_params, file = expB1_df_path, row.names = TRUE)
+  }
+  # Create simulated alignments
+  if (number_parallel_threads == 1) {
+    expB1_op_list <- lapply(
+      1:nrow(expB1_params),
+      random.trees.generate.alignment,
+      output_directory = expB1_dir,
+      iqtree2_path = iqtree2_path,
+      experiment_params = expB1_params
+    )
+  } else {
+    expB1_op_list <- mclapply(
+      1:nrow(expB1_params),
+      random.trees.generate.alignment,
+      output_directory = expB1_dir,
+      iqtree2_path = iqtree2_path,
+      experiment_params = expB1_params,
+      mc.cores = number_parallel_threads
+    )
+  }
+  # Record output alignment files
+  expB1_op_df <- as.data.frame(do.call(rbind, expB1_op_list))
+  expB1_op_df_path <- paste0(simulation_directory, "expB1_file_output_paths.csv")
+  write.csv(expB1_op_df, file = expB1_op_df_path, row.names = TRUE)
+}
+
+
+
+## Experiment B2: Mimicking ILS and introgression with a Yule tree ##
+# Generate gene trees using ms to simulate ILS with a single introgression event
+#   - Fix alignment at total_alignment_length
+#   - Simulate gene trees for tree containing a single introgression event in ms.
+#     Vary proportion of introgressed DNA from 0 to 1
+#   - Simulate DNA along each tree with Alisim
+#   - Concatenate alignments
+if (run.experiment.B2 == TRUE) {
+  # Create folder to store results of this experiment
+  expB2_dir <- paste0(simulation_directory, "exp_B2/")
+  if (dir.exists(expB2_dir) == FALSE) {
+    dir.create(expB2_dir)
+  }
+  # Create (or open if it exists) dataframe of simulation parameters
+  expB2_df_path <- paste0(simulation_directory, "expB2_parameters.csv")
+  if (file.exists(expB2_df_path) == TRUE) {
+    expB2_params <- read.csv(expB2_df_path)
+  } else {
+    expB2_params <- expand.grid(
+      "num_reps" = number_of_replicates,
+      "num_taxa" = number_of_taxa,
+      "num_trees" = number_gene_trees,
+      "tree_age" = tree_age,
+      "recombination_value" = r_vec,
+      "recombination_type" = c("Ancient", "Recent"),
+      "speciation_rate" = speciation_rates
+    )
+    # Add conversion to substitutions per site
+    expB2_params$tree_depth_subspersite <- expB2_params$tree_age
+    expB2_params$tree_depth_subspersite[which(expB2_params$tree_age == 5)] <- 0.005
+    expB2_params$tree_depth_subspersite[which(expB2_params$tree_age == 50)] <- 0.05
+    expB2_params$tree_depth_subspersite[which(expB2_params$tree_age == 500)] <- 0.5
+    # Add a unique identifier (uid) of the form:
+    # expB2_{num. trees}_{num. taxa}_{rep. num.}_{tree age}_{recombination prop.}_{event type}_{speciation_rate}
+    expB2_params$uid <- paste0(
+      "expB2_",
+      sprintf("%05d", expB2_params$num_trees),
+      "_",
+      sprintf("%04d", expB2_params$num_taxa),
+      "_",
+      sprintf("%03d", expB2_params$num_reps),
+      "_",
+      expB2_params$tree_age,
+      "_",
+      expB2_params$recombination_value,
+      "_",
+      expB2_params$recombination_type,
+      "_",
+      expB2_params$speciation_rate
+    )
+    # Add parameters for Alisim
+    expB2_params$alisim_gene_models <- alisim_gene_models
+    expB2_params$alisim_gene_tree_length <- gene_length
+    # Add other parameters
+    expB2_params$total_alignment_length <- expB2_params$num_trees * expB2_params$alisim_gene_tree_length
+    expB2_params$sequence_type <- sequence_type
+    # Add partition file and output alignment file for simulated alignments
+    expB2_params$partition_file <- paste0(expB2_params$uid, "_partitions.nex")
+    expB2_params$output_alignment_file <- paste0(expB2_params$uid, "_output_alignment")
+
+    # Remove any rows that have 5 taxa and an ancient introgression event
+    # Due to the way ancient introgression events are structured
+    #     (at time where four taxa exist, between two non-sister taxa),
+    #     recent and ancient introgression events are identical 5-taxon trees
+    remove_rows <- which(expB2_params$num_taxa == 5 &
+                           expB2_params$recombination_type == "Ancient")
+    keep_rows <- setdiff(1:nrow(expB2_params), remove_rows)
+    expB2_params <- expB2_params[keep_rows, ]
+    row.names(expB2_params) <- 1:nrow(expB2_params)
+    # Write expB2_params dataframe to file as a csv
+    write.csv(expB2_params, file = expB2_df_path, row.names = TRUE)
+  }
+  # Create simulated alignments
+  if (number_parallel_threads == 1) {
+    expB2_op_list <- lapply(
+      1:nrow(expB2_params),
+      ms.generate.alignment,
+      output_directory = expB2_dir,
+      ms_path = ms_path,
+      iqtree2_path = iqtree2_path,
+      experiment_params_df = expB2_params,
+      select.sister = FALSE,
+      scale.gene.trees = TRUE
+    )
+  } else {
+    expB2_op_list <- mclapply(
+      1:nrow(expB2_params),
+      ms.generate.alignment,
+      output_directory = expB2_dir,
+      ms_path = ms_path,
+      iqtree2_path = iqtree2_path,
+      experiment_params_df = expB2_params,
+      select.sister = FALSE,
+      scale.gene.trees = TRUE,
+      mc.cores = number_parallel_threads
+    )
+    expB2_op_list <- mclapply(
+      1:5,
+      ms.generate.alignment,
+      output_directory = expB2_dir,
+      ms_path = ms_path,
+      iqtree2_path = iqtree2_path,
+      experiment_params_df = expB2_params,
+      select.sister = FALSE,
+      scale.gene.trees = TRUE,
+      mc.cores = number_parallel_threads
+    )
+
+  }
+  # Record output alignment files
+  expB2_op_df <- as.data.frame(do.call(rbind, expB2_op_list))
+  expB2_op_df_path <- paste0(simulation_directory, "expB2_file_output_paths.csv")
+  write.csv(expB2_op_df, file = expB2_op_df_path, row.names = TRUE)
+}
+
+

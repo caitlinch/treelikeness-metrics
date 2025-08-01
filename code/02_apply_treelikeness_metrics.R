7ecd861c7fc691290b72470b4a69bce2472392ec
Caitlin Cherryh <24237077+caitlinch@users.noreply.github.com>
2025-08-01 10:10:34 +1000

    Run first 20 reps for all params.

--- code/02_apply_treelikeness_metrics.R
+++ code/02_apply_treelikeness_metrics.R
@@ -108,8 +108,8 @@ if (run_exp1 == TRUE){
                            invert = TRUE
                          ))
   exp1_op_df <- read.csv(exp1_op_file, stringsAsFactors = FALSE)
-  # Reduce to only first 10 reps for each set of simulation parameters
-  exp1_op_df <- exp1_op_df[which(exp1_op_df$num_reps <= 10), ]
+  # Reduce to only first 20 reps for each set of simulation parameters
+  exp1_op_df <- exp1_op_df[which(exp1_op_df$num_reps <= 20), ]
   # Extract alignments
   exp1_als <- exp1_op_df$output_alignment_file
   # Apply treelikeness metrics to all alignments
